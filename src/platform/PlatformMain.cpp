5ec6ca3d227e5d1c23fed5015977721dea48ad30
ObiJello <150304808+ObiJello@users.noreply.github.com>
2025-07-01 12:19:42 -0400

    Update

--- src/platform/PlatformMain.cpp
+++ src/platform/PlatformMain.cpp
@@ -587,6 +587,7 @@ namespace PlatformMain {
 
             // k) Clear buffers
             glClearColor(0.5f, 0.7f, 1.0f, 1.0f); // Sky blue
+            //glClearColor(0.1f, 0.1f, 0.15f, 1.0f); // Sky purple
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             // l) Calculate frustum for chunk visibility testing
