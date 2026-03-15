0b991e58b683eb51fd211c24e6e05a0271e0e1f0
ObiJello <obeykadar1@gmail.com>
2026-03-15 13:09:25 -0400

    Removed bump_version.ps1 still trying to edit Config.hpp on Windows builds

--- tools/bump_version.ps1
+++ tools/bump_version.ps1
@@ -18,15 +18,10 @@ if ($Type -eq "launcher") {
     $Config = "$SourceRoot/src/launcher/LauncherConfig.hpp"
     $Pattern = 'LauncherVersion = "[^"]*"'
     $Replacement = "LauncherVersion = `"$Version`""
-} else {
-    $Config = "$SourceRoot/src/common/core/Config.hpp"
-    $Pattern = 'GAME_VERSION "[^"]*"'
-    $Replacement = "GAME_VERSION `"$Version`""
+    $content = [System.IO.File]::ReadAllText($Config)
+    $content = [System.Text.RegularExpressions.Regex]::Replace($content, $Pattern, $Replacement)
+    [System.IO.File]::WriteAllText($Config, $content)
 }
-
-$content = [System.IO.File]::ReadAllText($Config)
-$content = [System.Text.RegularExpressions.Regex]::Replace($content, $Pattern, $Replacement)
-[System.IO.File]::WriteAllText($Config, $content)
 
 Write-Host "[bump-version] $Type version -> $Version"
 
